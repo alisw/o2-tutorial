cf456ac2b3dd02e981ae01725657e5e3edec91e8
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-12-19 14:26:37 +0800

    Bring O2 Tutorial back in business
    * Now works with elm 0.18.0.
    * Updated to match the latest `aliBuild` deployments.
    * Travis-CI tests and automated publishing.
    * Nix build configuration.

--- DataModel.elm
+++ DataModel.elm
@@ -1,79 +1,154 @@
-module DataModel where
+module DataModel exposing (..)
+
 import Html exposing (..)
 import String exposing (join)
-import List exposing (length)
+import List exposing (length, map)
 import Array exposing (Array, get)
 
-type alias Background = {
-  template: Html
-}
 
-type Pane = ShellPane { content: List BuildUp }
-          | EditorPane { content: List BuildUp, filename : String }
-          | HeaderPane { content: List BuildUp }
+type alias Background msg =
+    { template : Html msg
+    }
 
-type Layout = TwoPanesStep {
-                header : Pane,
-                leftPane : Pane,
-                rightPane : Pane
-              }
-            | SinglePaneStep {
-                header : Pane,
-                pane: Pane
-              }
-            | ImageStep {
-                header : Pane,
-                image : String
-              }
 
-{-- Buildups are used to specify incrementally contents of one slide, in
+type Pane
+    = ShellPane { content : List BuildUp }
+    | EditorPane { content : List BuildUp, filename : String }
+    | HeaderPane { content : List BuildUp }
+
+
+
+{--
+A story is the toplevel name for our tutorial
+--}
+
+
+type alias Story =
+    { title : String
+    , layouts : List Layout
+    }
+
+
+
+{--Layout is a description of the layout of the current slide
+--}
+
+
+type Layout
+    = TwoPanesStep
+        { title : String
+        , header : Pane
+        , leftPane : Pane
+        , rightPane : Pane
+        }
+    | SinglePaneStep
+        { title : String
+        , header : Pane
+        , pane : Pane
+        }
+    | ImageStep
+        { title : String
+        , header : Pane
+        , image : String
+        }
+
+
+
+{--Buildups are used to specify incrementally contents of one slide, in
     order to avoid having to repeat over and over the same content just
     to show small additions. Notice that you will be responsible for making
-    sure that different parts of the slide have buildups in sync, by using 
+    sure that different parts of the slide have buildups in sync, by using
     the dummy "Reuse" buildup.
 --}
-type BuildUp = Reuse
-             | Single String
-             | Replace String
-             | Append String
-             | ReplaceLast String
-             | ReplaceLastN Int String
 
-foldBuildup : BuildUp -> List String -> List String 
-{-- Given a list of buildups, it calculates the string corresponding to folding
+
+type BuildUp
+    = Reuse
+    | Single String
+    | Replace String
+    | Append String
+    | AppendP String
+    | ReplaceLast String
+    | ReplaceLastN Int String
+
+
+simpleBuildUp : List String -> List BuildUp
+simpleBuildUp l =
+    case l of
+        [] ->
+            []
+
+        [ x ] ->
+            [ Single x ]
+
+        x :: xs ->
+            [ Single x ] ++ (List.map Append xs)
+
+
+
+{--Given a list of buildups, it calculates the string corresponding to folding
     each buildup one after the other.
  --}
-foldBuildup next acc =
-  case next of
-    Reuse           -> acc
-    Single a        -> [a]
-    Replace a       -> [a]
-    Append a        -> acc ++ [a]
-    ReplaceLast a  -> (List.take ((List.length acc) - 1) acc) ++ [a]
-    ReplaceLastN n a  -> (List.take ((List.length acc) - n) acc) ++ [a]
 
-maxSlideBuildUp : Int -> Array Layout -> Int
-maxSlideBuildUp i slides =
-  maxBuildUp (get i slides) - 1
- 
+
+foldBuildup : BuildUp -> List String -> List String
+foldBuildup next acc =
+    case next of
+        Reuse ->
+            acc
+
+        Single a ->
+            [ a ]
+
+        Replace a ->
+            [ a ]
+
+        Append a ->
+            acc ++ [ a ]
+
+        AppendP a ->
+            acc ++ [ "\n\n" ++ a ]
+
+        ReplaceLast a ->
+            (List.take ((List.length acc) - 1) acc) ++ [ a ]
+
+        ReplaceLastN n a ->
+            (List.take ((List.length acc) - n) acc) ++ [ a ]
+
+
 maxBuildUp : Maybe Layout -> Int
 maxBuildUp layout =
-  case layout of
-    Just (TwoPanesStep a) -> max (maxPaneBuildUp a.leftPane) (maxPaneBuildUp a.rightPane)
-    Just (SinglePaneStep a) -> maxPaneBuildUp a.pane
-    Just (ImageStep a) -> 1
-    Nothing -> 1
+    case layout of
+        Just (TwoPanesStep a) ->
+            max (maxPaneBuildUp a.leftPane) (maxPaneBuildUp a.rightPane)
+
+        Just (SinglePaneStep a) ->
+            maxPaneBuildUp a.pane
+
+        Just (ImageStep a) ->
+            1
+
+        Nothing ->
+            1
+
 
 maxPaneBuildUp : Pane -> Int
 maxPaneBuildUp pane =
-  case pane of
-    ShellPane pane -> length pane.content
-    EditorPane pane -> length pane.content
-    HeaderPane pane -> length pane.content
+    case pane of
+        ShellPane pane ->
+            length pane.content
+
+        EditorPane pane ->
+            length pane.content
+
+        HeaderPane pane ->
+            length pane.content
+
 
 evaluateBuildUp : Int -> List BuildUp -> String
 evaluateBuildUp n allBuildups =
-  let
-    buildups = List.take (n+1) allBuildups
-  in
-    join "" (List.foldl foldBuildup [] buildups)
+    let
+        buildups =
+            List.take (n + 1) allBuildups
+    in
+        join "" (List.foldl foldBuildup [] buildups)
